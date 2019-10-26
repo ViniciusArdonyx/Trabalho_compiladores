a7127cd2f8aff197af50280db0c5341a47cc7a45
viniciusms29 <40607859+viniciusms29@users.noreply.github.com>
2019-10-26 16:45:28 -0300

    Criacao Analisador Sintatico

--- Sintatico.py
+++ Sintatico.py
@@ -4,7 +4,7 @@ import Token
 import TokensClass
 import Lexico
 class Sintatico:
-    # teste
+    # teste 3
     def __init__(self):
         self.analisadorLexico = Lexico.Lexico
         self.tokenAtual = None
